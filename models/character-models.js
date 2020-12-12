f5da909b8a5f89ecb42a8d2daee55a547ef4c587
Zac Smith <zrs3141592@gmail.com>
2020-12-12 13:06:16 -0700

    update models

--- models/character-models.js
+++ models/character-models.js
@@ -9,13 +9,26 @@ module.exports = {
   remove,
 }
 
-function add() {}
+async function add(character) {
+  const [id] = await db('characters').insert(character)
+  return findById(id)
+}
 
 function find() {
   return db('characters')
 }
 
-function findBy() {}
-function findById() {}
-function update() {}
-function remove() {}
+function findBy(filter) {
+  return db('character').where(filter)
+}
+
+function findById(id) {
+  return db('characters').where({ id }).first()
+}
+function update(changes, id) {
+  return db('characters').where({ id }).update(changes)
+}
+
+function remove(id) {
+  return db('characters').where('id', Number(id)).del()
+}
--- routes/character-routes.js
+++ routes/character-routes.js
@@ -2,7 +2,24 @@ const router = require('express').Router()
 const Characters = require('../models/character-models.js')
 
 router.get('/', (req, res) => {
-  res.send('GET characters!!!')
+  Characters.find()
+    .then((characters) => res.status(200).json(characters))
+    .catch((err) => {
+      console.log(`Error: ${error}`.red)
+      res.status(500).json({
+        message: err,
+      })
+    })
 })
+
+// GET character/:id
+
+// POST character
+
+// PUT character/:id
+
+// DEL charactger/:id
+
+// GET charcter/:filter
 
 module.exports = router
