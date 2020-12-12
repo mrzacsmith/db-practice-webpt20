702cc29ba23685e92be863f18e7150c80da2949f
Zac Smith <zrs3141592@gmail.com>
2020-12-12 12:20:28 -0700

    basic model

---
+++ models/character-models.js
@@ -0,0 +1,21 @@
+const db = require('../database/dbConfig.js')
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove,
+}
+
+function add() {}
+
+function find() {
+  return db('characters')
+}
+
+function findBy() {}
+function findById() {}
+function update() {}
+function remove() {}
